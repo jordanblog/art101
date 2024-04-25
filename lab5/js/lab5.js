fc28812d94acc0e0179cdf1ebb2879b0f2e4ca12
jordanblog <jordanmprice02@gmail.com>
2024-04-25 16:45:52 -0700

    updated index

---
+++ lab5/js/lab5.js
@@ -0,0 +1,44 @@
+// index.js - Lab 5
+// Author: Jordan Price
+// Date: April 25, 2024
+
+// Define Variables
+make = "Subaru"
+model = "Baja"
+color = "Black"
+year = 2006
+
+//calculate
+age = 2024 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age:" + age + " years<br>");
+
+
+
+
+
+
+
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
+function main() {
+  console.log("Main function started.");
+  // the code that makes everything happen
+}
+
+// let's get this party started
+main();
