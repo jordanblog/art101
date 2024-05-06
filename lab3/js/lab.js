bca661d88f0949e4992a8c0455eb6e9557785786
jordanblog <jordanmprice02@gmail.com>
2024-05-06 11:44:02 -0700

    new homepage

--- lab3/js/lab.js
+++ lab3/js/lab.js
@@ -6,16 +6,9 @@
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function sayMyName() {
+  return "Jordan";
 }
+document.write("My name is " + sayMyName());
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
-main();
