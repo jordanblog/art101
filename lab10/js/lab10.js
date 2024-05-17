16c6d68d2fc5193ac2ac711288ff5af175bb6c8e
jordanblog <jordanmprice02@gmail.com>
2024-05-16 18:49:10 -0700

    lab 10 comp

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -7,23 +7,23 @@
    Date: 2024
 */
 
+
+// Function to generate random text
 function generateRandomText() {
-    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const min = 3;
-    const max = 100;
-    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Get a random starting index to slice the Lorem Ipsum text
-    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-    // Generate the random Lorem Ipsum-like text
-    return text.slice(randStart, randStart + randLen);
-  }
+  const text = ["Purple", "Blue", "Magenta", "Green", "Orange", "Red", "Lavender", "White", "Beige", "Amber",]
+  const min = 1;
+  const max = 1;
+  const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+  // Get a random starting index to slice the Lorem Ipsum text
+  const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+  // Generate the random Lorem Ipsum-like text
+  return text.slice(randStart, randStart + randLen);
+}
 
-// click listener for button
+// Click listener for button
 $("#make-convo").click(function(){
+  // Get new fake dialogue
+  const newText = generateRandomText();
+  // Replace the content of the output div with new text
+  $("#output").html('<div class="text"><p>' + newText + '</p></div>');
 });
-
-// get new fake dialogue
-const newText = generateRandomText();
-
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
